76ad3c5597d4c8c1dc7feac0bcdb80a7be868a9d
Price Comstock <price.comstock@gmail.com>
2020-04-05 16:11:05 -0400

    :building_construction: rearranged socket stuff

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const http = require("http");
 const axios = require("axios");
-const io = require("./socket/socket"); // configuration for socket.io
+const socketIo = require("socket.io");
+
+const initializeSocketIO = require("./socket/socket"); // configuration for socket.io
 
 const port = process.env.PORT || 4001;
 
@@ -13,3 +15,7 @@ app.use(index);
 app.use("/api", api);
 
 const server = http.createServer(app);
+const io = socketIo(server);
+initializeSocketIO(io);
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
--- server/socket/socket.js
+++ server/socket/socket.js
@@ -1,10 +1,10 @@
 const socketIo = require("socket.io");
 
-const io = socketIo(server);
+function initialize(io) {
+  io.on("connection", (socket) => {
+    console.log("New client connected");
+    socket.on("disconnect", () => console.log("Client disconnected"));
+  });
+}
 
-io.on("connection", (socket) => {
-  console.log("New client connected");
-  socket.on("disconnect", () => console.log("Client disconnected"));
-});
-
-module.exports = io;
+module.exports = initialize;
