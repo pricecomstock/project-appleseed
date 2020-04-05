1233ecd5e2b2fcafe7a0c8aaf86d0c398a277692
Price Comstock <price.comstock@gmail.com>
2020-04-05 16:53:46 -0400

    :sparkles: basic mockup of landing page

--- src/App.js
+++ src/App.js
@@ -5,20 +5,14 @@ import "./mvp.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Project Appleseed</h1>
       </header>
+      <section>
+        <h2>Enter room code:</h2>
+        <label for="roomcode">Code:</label>
+        <input type="text" id="roomcode" />
+      </section>
     </div>
   );
 }
