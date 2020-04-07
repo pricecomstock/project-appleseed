7464a615523cab8b22cf7c30d0b9dcc4d3d28823
Price Comstock <price.comstock@gmail.com>
2020-04-07 18:49:54 -0400

    set up basic routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./mvp.css";
 import "./appleseed.scss";
 
 import Home from "./components/pages/Home";
+import HostView from "./components/pages/HostView";
+import PlayerView from "./components/pages/PlayerView";
 
 function App() {
   return (
-    <div className="App section">
-      <Home />
-    </div>
+    <Router>
+      <div className="App section">
+        {/* notice "exact" */}
+        <Route exact path="/" component={Home}></Route>
+
+        <Route exact path="/watch" component={HostView}></Route>
+        <Route exact path="/play" component={PlayerView}></Route>
+      </div>
+    </Router>
   );
 }
 
