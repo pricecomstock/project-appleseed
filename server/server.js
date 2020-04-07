9ef6b2f34e977e7bac3ae4ff3095d8198c2eb025
Price Comstock <price.comstock@gmail.com>
2020-04-07 18:25:42 -0400

    cors fix

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const http = require("http");
+const cors = require("cors");
 const axios = require("axios");
 const socketIo = require("socket.io");
 const createRouter = require("./routes/router.js");
@@ -18,16 +19,17 @@ const api = createRouter(io);
 
 // Very open CORS rules
 // TODO figure out production version of this
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  next();
-});
+app.options("*", cors());
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   next();
+// });
 
 app.use(index);
-app.use("/api", api);
+app.use("/api", cors(), api);
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
--- src/axios-backend.js
+++ src/axios-backend.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Should just be /api if env variable isn't set, which means it will request its own domain
 // const baseURL = process.env.BACKEND_URL + "/api";
-const baseURL = "localhost:4001/api";
+const baseURL = "http://localhost:4001/api";
 
 const instance = axios.create({
   baseURL: baseURL,
