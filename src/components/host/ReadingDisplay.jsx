9c07ef05216a188a13d8f0f2fc1c92dfd9feff9b
Price Comstock <price.comstock@gmail.com>
2020-04-18 16:35:36 -0400

    made nice little voting display

--- src/components/host/ReadingDisplay.jsx
+++ src/components/host/ReadingDisplay.jsx
@@ -5,17 +5,20 @@ export default class ReadingDisplay extends Component {
     return (
       <div>
         <h1 className="title">{this.props.prompt.text}</h1>
-        <p>{this.props.prompt.id}</p>
-        <p></p>
-        {this.props.prompt.answers &&
-          this.props.prompt.answers.map((answer) => {
-            return (
-              <div className="box">
-                <h3 className="subtitle">{answer[1]}</h3>
-                <span className="tag is-light is-warning">{answer[0]}</span>
-              </div>
-            );
-          })}
+        {/* <p>{this.props.prompt.id}</p> */}
+        <div className="columns is-centered is-vcentered is-multiline">
+          {this.props.prompt.answers &&
+            this.props.prompt.answers.map((answer) => {
+              return (
+                <div className="column is-one-third">
+                  <div className="box prompt-box">
+                    <h3 className="subtitle">{answer[1]}</h3>
+                    <span className="tag is-light is-warning">{answer[0]}</span>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
       </div>
     );
   }
