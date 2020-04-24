99ae7a03cf6fb2942c603490a37547b5303044f3
Price Comstock <price.comstock@gmail.com>
2020-04-24 15:41:16 -0400

    :sparkles: made temporary sheety db sync script

---
+++ server/game/prompts/syncFromSheety.js
@@ -0,0 +1,21 @@
+const axios = require("axios");
+const fs = require("fs");
+
+const sheetyUrl =
+  "https://v2-api.sheety.co/c732e4da3ff5d7345f492f050ab149e9/appleseed/prompts";
+const fileName = "./server/game/prompts/sheety.json";
+
+function sync() {
+  axios
+    .get(sheetyUrl)
+    .then((res) => {
+      let prompts = res.data.map;
+      fs.writeFileSync(fileName, JSON.stringify(prompts));
+    })
+    .catch((e) => {
+      console.error("Error:", e);
+      return;
+    });
+}
+
+sync();
