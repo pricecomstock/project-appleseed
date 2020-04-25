a7b07d63e01ded7a3324c800618a2ddedb2eb4d7
Price Comstock <price.comstock@gmail.com>
2020-04-25 12:25:01 -0400

    :sparkles: made player name nicer to look at

--- src/components/host/ReadingDisplay.jsx
+++ src/components/host/ReadingDisplay.jsx
@@ -4,59 +4,79 @@ export default class ReadingDisplay extends Component {
   render() {
     return (
       <div>
-        <h1 className="title">{this.props.prompt.text}</h1>
+        <div className="hero is-info">
+          <div class="hero-body">
+            <div class="container has-text-centered">
+              <h1 className="is-size-1">{this.props.prompt.text}</h1>
+            </div>
+          </div>
+        </div>
         {/* <p>{this.props.prompt.id}</p> */}
-        <div className="columns is-centered is-multiline">
-          {this.props.prompt.answers &&
-            this.props.prompt.answers.map((answer, answerIndex) => {
-              return (
-                <div className="column is-one-third" key={answerIndex}>
-                  <div className="box">
-                    <h1 className="title">{answer[1]}</h1>
-                    {/* <span className="tag is-light is-small">{answer[0]}</span> */}
-                  </div>
-                  {this.props.votingIsComplete && (
-                    <div>
-                      <span className="tag is-success is-light is-large">
-                        +{" "}
-                        {this.props.scoringDetails &&
-                          this.props.scoringDetails.pointsArray[answerIndex]}
-                      </span>
-                      {this.props.scoringDetails.isShutout &&
-                        this.props.scoringDetails.shutoutIndex ===
-                          answerIndex && (
-                          <span className="tag is-success is-large">
-                            + {this.props.scoringDetails.shutoutPoints} for
-                            shutout!
-                          </span>
-                        )}
-                      <div className="tags">
-                        {Object.entries(this.props.votingResults).map(
-                          (entry) => {
-                            let playerId = entry[0];
-                            let voteIndex = entry[1];
-
-                            if (voteIndex === answerIndex) {
-                              return (
-                                <span className="tag is-info is-medium is-light">
-                                  {/* FIXME this is probably very bad performance */}
-                                  {this.props.getPlayerInfoById(playerId).emoji}
-                                  &nbsp;
-                                  {
-                                    this.props.getPlayerInfoById(playerId)
-                                      .nickname
-                                  }
-                                </span>
-                              );
-                            }
-                          }
-                        )}
-                      </div>
+        <div className="container">
+          <div className="columns is-centered is-multiline">
+            {this.props.prompt.answers &&
+              this.props.prompt.answers.map((answer, answerIndex) => {
+                return (
+                  <div className="column is-one-third" key={answerIndex}>
+                    <div className="box has-text-centered">
+                      <h3 className="is-size-2">{answer[1]}</h3>
+                      {this.props.votingIsComplete && (
+                        <span className="tag is-info is-large">
+                          {this.props.getPlayerInfoById(answer[0]).emoji}{" "}
+                          {this.props.getPlayerInfoById(answer[0]).nickname}
+                        </span>
+                      )}
                     </div>
-                  )}
-                </div>
-              );
-            })}
+                    {this.props.votingIsComplete && (
+                      <div>
+                        <div className="tags">
+                          <span className="tag is-success is-large">
+                            +{" "}
+                            {this.props.scoringDetails &&
+                              this.props.scoringDetails.pointsArray[
+                                answerIndex
+                              ]}
+                          </span>
+                          {this.props.scoringDetails.isShutout &&
+                            this.props.scoringDetails.shutoutIndex ===
+                              answerIndex && (
+                              <span className="tag is-success is-light is-large">
+                                + {this.props.scoringDetails.shutoutPoints} for
+                                shutout!
+                              </span>
+                            )}
+                        </div>
+                        <div className="tags">
+                          {Object.entries(this.props.votingResults).map(
+                            (entry) => {
+                              let playerId = entry[0];
+                              let voteIndex = entry[1];
+
+                              if (voteIndex === answerIndex) {
+                                return (
+                                  <span className="tag is-medium is-light">
+                                    {/* FIXME this is probably very bad performance */}
+                                    {
+                                      this.props.getPlayerInfoById(playerId)
+                                        .emoji
+                                    }
+                                    &nbsp;
+                                    {
+                                      this.props.getPlayerInfoById(playerId)
+                                        .nickname
+                                    }
+                                  </span>
+                                );
+                              }
+                            }
+                          )}
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                );
+              })}
+          </div>
         </div>
       </div>
     );
--- src/components/player/PlayerInfoView.jsx
+++ src/components/player/PlayerInfoView.jsx
@@ -3,14 +3,14 @@ import React, { Component } from "react";
 export default class PlayerInfoView extends Component {
   render() {
     return (
-      <div className="tags has-addons are-medium">
-        <span className="tag is-info">
+      <div className="tags has-addons are-large">
+        <span className="tag is-light is-info">
           {this.props.playerInfo.emoji}&nbsp;
           {this.props.playerInfo.nickname}
         </span>
         {this.props.canEdit && (
           <span
-            className="tag is-light is-info"
+            className="tag is-light is-warning"
             onClick={this.props.onEdit}
             style={{ cursor: "pointer" }}
           >
