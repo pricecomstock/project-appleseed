d53a371190fa5102d80f0d5659486b59b1559ad5
Price Comstock <price.comstock@gmail.com>
2020-04-27 22:06:38 -0400

    added docker deploy script

---
+++ deploydocker.sh
@@ -0,0 +1,31 @@
+
+function push {
+  docker tag bitwits pricecomstock/bitwits
+  docker push pricecomstock/bitwits:latest
+}
+
+function testcontainer {
+  docker run -p 5000:80 bitwits > /dev/null &
+  echo "Do you still wish to deploy?"
+  select yn in "Yes" "No"; do
+      case $yn in
+          Yes ) push; break;;
+          No ) break;;
+      esac
+  done
+}
+
+docker build --tag=bitwits .
+echo ""
+echo "=============================="
+echo ""
+echo "Container built!"
+echo "You can either launch a test container on port 5000, or throw caution to the wind and just push to DockerHub"
+echo "Please choose next step:"
+select next in "Test" "Push" "Nothing"; do
+  case $next in
+    Test ) testcontainer; break;;
+    Push ) push; break;;
+    Nothing ) break;;
+  esac
+done
