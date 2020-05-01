382c989b892380caa7c5955a4b16067c1a66ec1c
Price Comstock <price.comstock@gmail.com>
2020-05-01 14:17:32 -0400

    :sparkles: Players can view prompt when voting

--- src/components/player/PlayerVote.jsx
+++ src/components/player/PlayerVote.jsx
@@ -3,20 +3,25 @@ import React, { Component } from "react";
 export default class PlayerVote extends Component {
   render() {
     return (
-      <div className="buttons">
-        {this.props.currentVotingMatchup.answers &&
-          this.props.currentVotingMatchup.answers.map((answer, index) => {
-            return (
-              <button
-                className="button is-medium is-fullwidth is-outlined wrapped-text"
-                onClick={() => {
-                  this.props.submitVote(index);
-                }}
-              >
-                <p>{answer[1]}</p>
-              </button>
-            );
-          })}
+      <div className="container">
+        <div className="has-text-centered is-size-4">
+          {this.props.currentVotingMatchup.text}
+        </div>
+        <div className="buttons">
+          {this.props.currentVotingMatchup.answers &&
+            this.props.currentVotingMatchup.answers.map((answer, index) => {
+              return (
+                <button
+                  className="button is-medium is-fullwidth is-outlined wrapped-text"
+                  onClick={() => {
+                    this.props.submitVote(index);
+                  }}
+                >
+                  <p>{answer[1]}</p>
+                </button>
+              );
+            })}
+        </div>
       </div>
     );
   }
