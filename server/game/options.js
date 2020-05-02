22ad0ed36552f0ee01b42c57d3a1d865a25d3eed
Price Comstock <price.comstock@gmail.com>
2020-05-02 16:21:11 -0400

    :sparkles: x10 was TOO POWERFUL

--- server/game/options.js
+++ server/game/options.js
@@ -3,8 +3,8 @@ const { answersPerPromptOptions } = require("./prompts/prompt");
 const defaultOptions = {
   maxPlayers: 16,
 
-  pointsPerPrompt: 12000,
-  pointsForShutout: 2000,
+  pointsPerPrompt: 6000,
+  pointsForShutout: 1000,
 
   rounds: [
     {
