b0221b617c3f0edfa51671197a894430b5b26157
Price Comstock <price.comstock@gmail.com>
2020-05-09 06:38:50 -0400

    :recycle: refactored readingdisplay with AnswerCard components

--- src/components/host/AnswerCard.jsx
+++ src/components/host/AnswerCard.jsx
@@ -3,10 +3,11 @@ import classNames from "classnames";
 
 export default class AnswerCard extends Component {
   render() {
+    console.log("voters", this.props.voters);
     return (
       <div className="">
         <div className="box has-text-centered">
-          <h3 className={classNames(this.textClass())}>{this.props.text}</h3>
+          <h3 className="is-size-4">{this.props.text}</h3>
           {this.props.votingIsComplete && (
             <span className="tag is-info is-large">
               {this.props.playerData.emoji} {this.props.playerData.nickname}
@@ -29,7 +30,6 @@ export default class AnswerCard extends Component {
               {this.props.voters.map((playerData, voterIndex) => {
                 return (
                   <span className="tag is-medium is-light" key={voterIndex}>
-                    {/* FIXME this is probably very bad performance */}
                     {playerData.emoji}
                     &nbsp;
                     {playerData.nickname}
--- src/components/host/ReadingDisplay.jsx
+++ src/components/host/ReadingDisplay.jsx
@@ -2,20 +2,6 @@ import React, { Component } from "react";
 import AnswerCard from "./AnswerCard";
 
 export default class ReadingDisplay extends Component {
-  textClass = () => {
-    if (this.props.prompt.answers) {
-      if (this.props.prompt.answers.length < 4) {
-        return ["is-size-3"];
-      } else if (this.props.prompt.answers.length < 8) {
-        return ["is-size-4"];
-      } else {
-        return ["is-size-5"];
-      }
-    } else {
-      return ["is-size-4"];
-    }
-  };
-
   render() {
     return (
       <div>
@@ -35,19 +21,24 @@ export default class ReadingDisplay extends Component {
                         playerData={this.props.getPlayerInfoById(answer[0])}
                         votingIsComplete={this.props.votingIsComplete}
                         basePoints={
+                          this.props.scoringDetails &&
                           this.props.scoringDetails.pointsArray[answerIndex]
                         }
                         isShutout={
+                          this.props.scoringDetails &&
                           this.props.scoringDetails.isShutout &&
                           this.props.scoringDetails.shutoutIndex === answerIndex
                         }
-                        shutoutPoints={this.props.scoringDetails.shutoutPoints}
-                        voters={this.props.votingResults
+                        shutoutPoints={
+                          this.props.scoringDetails &&
+                          this.props.scoringDetails.shutoutPoints
+                        }
+                        voters={Object.entries(this.props.votingResults)
                           .filter((entry) => {
                             return entry[1] === answerIndex;
                           })
                           .map((entry) => {
-                            this.props.getPlayerInfoById(entry[0]);
+                            return this.props.getPlayerInfoById(entry[0]);
                           })} // FIXME performance?
                       ></AnswerCard>
                     );
