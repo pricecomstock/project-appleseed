c8e1af1e9de92156cd075400a9ac4d2585a176a2
Price Comstock <price.comstock@gmail.com>
2020-06-04 18:11:41 -0400

    got basic setup in place for testing gameroom

--- server/__tests__/gameRoom.test.js
+++ server/__tests__/gameRoom.test.js
@@ -1,3 +1,24 @@
-test("placeholder", () => {
-  expect(1 + 2).toBe(3);
+const RoomManager = require("../game/roomManager.js");
+const socketIo = require("socket.io");
+
+var io = null;
+var rm = null;
+var room = null;
+
+beforeEach(() => {
+  // NOTE anything in this function must be saved into global scope for access by test
+  io = new socketIo();
+  rm = new RoomManager(io);
+  return rm.createNewRoom().then((newRoom) => {
+    room = newRoom;
+  });
+  // console.log("io", typeOf(io));
+  // console.log("rm", typeOf(rm));
+  // return io;
+});
+
+test("room is created and starts in lobby, allowed to join", () => {
+  expect(room).toBeDefined();
+  expect(room.state).toBe("lobby");
+  expect(room.allowedToJoin).toBe(true);
 });
